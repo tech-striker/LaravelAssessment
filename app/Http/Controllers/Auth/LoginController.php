37439150617567c7e5c6ad6d680589aaf1d0162b
Tech Striker <37535762+tech-striker@users.noreply.github.com>
2021-04-16 13:29:26 +0530

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\User;
+
 
 class LoginController extends Controller
 {
@@ -36,5 +40,31 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+
+        $validator = \Validator::make($request->all(), [
+            'email' => 'required|string|email',
+            'password' => 'required|string|between:8,15',
+            'remember_me' => 'boolean'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors(), 'message' => 'Validation failed.'], 422);
+        }
+
+        $data = User::where('email',$request->email)->first();
+
+        if($data->email_verified == 1){
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return redirect('login')->with('flash_message', 'You dont have registered properly!.')->with('flash_type', 'alert-info'); 
+            }
+            return redirect('home')->with(['user' => $data]); 
+        }else {
+            return redirect('login')->with('flash_message', 'You dont have registered properly!.')->with('flash_type', 'alert-info'); 
+        }
     }
 }
